db60a7bf02e818588470cd0175afa39b30fca6f5
vay-tk <vaytk001@gmail.com>
2025-09-27 12:37:27 +0000

    frontend domain connected

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000'],
+  origin: ['http://localhost:5173', 'http://localhost:3000', 'https://https://new-face-frontend-production.up.railway.app'],
   credentials: true
 }));
 app.use(express.json({ limit: '50mb' }));
